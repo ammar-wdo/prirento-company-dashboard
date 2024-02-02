499f406ef3c55fba6e3085bb17599d1e67dc88ae
PRIRENTO <prirento@wdodigitals-Mac-mini.local>
2024-02-02 14:03:04 +0400

    try

--- components/(car)/car-feed.tsx
+++ components/(car)/car-feed.tsx
@@ -1,55 +1,49 @@
+import { getCompanyEmail } from "@/lib/utils";
 
+import React from "react";
+import NoResult from "../no-result";
+import prisma from "@/lib/prisma";
+import CarCard from "./car-card";
 
-import { getCompanyEmail } from '@/lib/utils'
+type Props = {};
 
-import React from 'react'
-import NoResult from '../no-result'
-import prisma from '@/lib/prisma'
-import CarCard from './car-card'
+const CarFeed = async (props: Props) => {
+  const email = await getCompanyEmail();
 
-type Props = {}
-
-const CarFeed = async(props: Props) => {
-  
-const email = await getCompanyEmail()
-
-
-const cars = await prisma.car.findMany({
-    where:{
-        company:{
-            email:email as string
-        },
-        
-        
+  const cars = await prisma.car.findMany({
+    where: {
+      company: {
+        email: email as string,
+      },
     },
-    orderBy:{
-        createdAt:'desc'
-    }
-    ,include:{
-        carModel:{
-            include:{
-                carBrand:{
-                    select:{
-                        brand:true
-                    }
-                }
-            }
-        }
-    }
-}) 
-
-
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      carModel: {
+        include: {
+          carBrand: {
+            select: {
+              brand: true,
+            },
+          },
+        },
+      },
+    },
+  });
 
   return (
     <div>
-        {!cars.length && <NoResult title='No cars'/>}
-        {!!cars.length && <div className='flex gap-3 flex-wrap'>
-            {cars.map((car)=><div>
-               {<CarCard key={car.id} car={car}/>}
-            </div>)}
-            </div>}
+      {!cars.length && <NoResult title="No cars" />}
+      {!!cars.length && (
+        <div className="flex gap-3 flex-wrap">
+          {cars.map((car) => (
+            <div>{<CarCard key={car?.id} car={car} />}</div>
+          ))}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default CarFeed
+export default CarFeed;
