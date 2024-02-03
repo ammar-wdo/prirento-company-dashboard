1da91b669a57e314e3daca434d67460b1d9e71ab
PRIRENTO <prirento@wdodigitals-Mac-mini.local>
2024-02-03 12:58:06 +0400

    extra options done

---
+++ app/dashboard/cars/[carId]/extra-options/[extraOptionId]/template.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { motion } from 'framer-motion';
+
+
+
+
+
+
+type Props = {children:React.ReactNode}
+
+const template = ({children}: Props) => {
+  return (
+    <motion.div
+  initial={{y:20,opacity:0}}
+  animate={{y:0,opacity:1}}
+  transition={{ease:'easeInOut',duration:0.75}}
+    >{children}</motion.div>
+  )
+}
+
+export default template
