a42397f5e89a5067b215f2c3723b69e381ddac55
PRIRENTO <prirento@wdodigitals-Mac-mini.local>
2024-02-06 08:09:30 +0400

    refine

--- components/(settings)/company-settings-form.tsx
+++ components/(settings)/company-settings-form.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Category, Company } from "@prisma/client";
+import {  Company } from "@prisma/client";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -19,12 +19,13 @@ import "react-phone-input-2/lib/style.css";
 import { SingleImageDropzone } from "../single-image-dropezone";
 
 import ActionLoaderButton from "../action-loader-button";
-import { Day, daysOrder, useCompany } from "@/hooks/company-settings.hook";
-import TimeSelect from "../time-select";
+import {  daysOrder, useCompany } from "@/hooks/company-settings.hook";
+
 import { Checkbox } from "../ui/checkbox";
-import { cn, generateTimeSlots } from "@/lib/utils";
+
 import OpentimeComponent from "../opentime-component";
-import FormWrapper from "../form-wrapper";
+
+import FormSectionsWrapper from "../form-sections-wrapper";
 
 type Props = { company: Company };
 
@@ -53,256 +54,249 @@ const CompanySettingsForm = ({ company }: Props) => {
         onSubmit={form.handleSubmit(onSubmit)}
         className="flex flex-col gap-8"
       >
-        <FormWrapper>
-
-  
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>E-mail*</FormLabel>
-              <FormControl>
-                <Input placeholder="email" {...field} />
-              </FormControl>
-
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="newPassword"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>New password*</FormLabel>
-              <FormControl>
-                <Input type="password" placeholder="new password" {...field} />
-              </FormControl>
-
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="address"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Address*</FormLabel>
-              <FormControl>
-                <Input placeholder="address" {...field} />
-              </FormControl>
-
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-              </FormWrapper>
-
-<FormWrapper>
-
-
-        <FormField
-          control={form.control}
-          name="openingTime"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Operating hours*</FormLabel>
-              <FormControl>
-              <ul className="flex flex-col gap-2 w-full mt-3">
-  <li className="grid grid-cols-4 gap-4 font-medium">
-    <span className="text-xs md:text-base">Day</span>
-    <span className="text-xs md:text-base">Open time</span>
-    <span className="text-xs md:text-base">Close time</span>
-    <span className="text-xs md:text-base justify-self-center">Closed</span>
-  </li>
-  {daysOrder.map(day => {
-    // Assuming form.watch("openingTime") returns an object with the same structure as defaultOpeningTimes
-    const dayData = form.watch(`openingTime.${day}`);
-    return (
-      <OpentimeComponent
-        key={day}
-        closeTime={dayData.closeTime}
-        openTime={dayData.openTime}
-        day={day}
-        dropdownStatus={dropdownStatus}
-        setter={setter}
-        toggleClose={toggleClose}
-        toggleDropdown={toggleDropdown}
-        isClosed={dayData.closed}
-        isChecked={dayData.closed}
-      />
-    );
-  })}
-</ul>
-              </FormControl>
-
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-<FormField
-          control={form.control}
-          name="away"
-          render={({ field }) => (
-            <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4 shadow">
-              <FormControl>
-                <Checkbox
-                  checked={field.value}
-                  onCheckedChange={field.onChange}
-                />
-              </FormControl>
-              <div className="space-y-1 leading-none">
-                <FormLabel>
-                Away mode
-                </FormLabel>
-                <FormDescription>
-                Block all cars while checked
-                  
-                </FormDescription>
-              </div>
-            </FormItem>
-          )}
-        />
-        </FormWrapper>
-
-        <FormWrapper>
-
-      
-
-        <FormField
-          control={form.control}
-          name="phoneNumber"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Phone number*</FormLabel>
-              <FormControl>
-                <PhoneInput
-                  enableSearch={true}
-                  buttonStyle={{ border: "none" }}
-                  containerStyle={{
-                    borderRadius: "7px",
-                    paddingBlock: 3,
-                    width: "100%",
-                    border: "0.4px #ECECEC solid",
-                  }}
-                  inputStyle={{
-                    border: "none",
-                    width: "100%",
-                    backgroundColor: "transparent",
-                  }}
-                  value={form.getValues("phoneNumber")}
-                  onChange={(phone) => form.setValue("phoneNumber", phone)}
-                />
-              </FormControl>
-
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="whatsApp"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>WhatsApp number*</FormLabel>
-              <FormControl>
-                <PhoneInput
-                  enableSearch={true}
-                  buttonStyle={{ border: "none" }}
-                  containerStyle={{
-                    borderRadius: "7px",
-                    paddingBlock: 3,
-                    width: "100%",
-                    border: "0.4px #ECECEC solid",
-                  }}
-                  inputStyle={{
-                    border: "none",
-                    width: "100%",
-                    backgroundColor: "transparent",
-                  }}
-                  value={form.getValues("whatsApp")}
-                  onChange={(phone) => form.setValue("whatsApp", phone)}
-                />
-              </FormControl>
-
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-          </FormWrapper>
-          <FormWrapper>
-
-      
-
-        <FormField
-          control={form.control}
-          name="logo"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Logo*</FormLabel>
-              <div className="flex items-center gap-3 w-full flex-wrap">
+        <FormSectionsWrapper title="Profile info">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>E-mail*</FormLabel>
                 <FormControl>
-                  <SingleImageDropzone
-                    width={200}
-                    height={200}
-                    value={file}
-                    onChange={(file) => {
-                      setFile(file);
-                    }}
+                  <Input placeholder="email" {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="newPassword"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>New password*</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="new password"
+                    {...field}
                   />
                 </FormControl>
-                <Button
-                  disabled={!file || !!form.watch("logo")}
-                  type="button"
-                  onClick={uploadImage}
-                >
-                  Upload
-                </Button>
-              </div>
-              <ImagePlaceholder />
 
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <FormField
-          control={form.control}
-          name="gallary"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Add images</FormLabel>
-              <div className="flex items-center gap-3 w-full flex-wrap">
+          <FormField
+            control={form.control}
+            name="address"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Address*</FormLabel>
                 <FormControl>
-                  <SingleImageDropzone
-                    width={200}
-                    height={200}
-                    value={imagesFile}
-                    onChange={(imagesFile) => {
-                      setImagesFile(imagesFile);
-                    }}
+                  <Input placeholder="address" {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </FormSectionsWrapper>
+
+        <FormSectionsWrapper title="Working time">
+          <FormField
+            control={form.control}
+            name="openingTime"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Operating hours*</FormLabel>
+                <FormControl>
+                  <ul className="flex flex-col gap-2 w-full mt-3">
+                    <li className="grid grid-cols-4 gap-4 font-medium">
+                      <span className="text-xs md:text-base">Day</span>
+                      <span className="text-xs md:text-base">Open time</span>
+                      <span className="text-xs md:text-base">Close time</span>
+                      <span className="text-xs md:text-base justify-self-center">
+                        Closed
+                      </span>
+                    </li>
+                    {daysOrder.map((day) => {
+                      // Assuming form.watch("openingTime") returns an object with the same structure as defaultOpeningTimes
+                      const dayData = form.watch(`openingTime.${day}`);
+                      return (
+                        <OpentimeComponent
+                          key={day}
+                          closeTime={dayData.closeTime}
+                          openTime={dayData.openTime}
+                          day={day}
+                          dropdownStatus={dropdownStatus}
+                          setter={setter}
+                          toggleClose={toggleClose}
+                          toggleDropdown={toggleDropdown}
+                          isClosed={dayData.closed}
+                          isChecked={dayData.closed}
+                        />
+                      );
+                    })}
+                  </ul>
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="away"
+            render={({ field }) => (
+              <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4 shadow">
+                <FormControl>
+                  <Checkbox
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
                   />
                 </FormControl>
-                <Button
-                  disabled={!imagesFile}
-                  type="button"
-                  onClick={uploadImages}
-                >
-                  Upload
-                </Button>
-              </div>
-              <ImagesPlaceholder />
+                <div className="space-y-1 leading-none">
+                  <FormLabel>Away mode</FormLabel>
+                  <FormDescription>
+                    Block all cars while checked
+                  </FormDescription>
+                </div>
+              </FormItem>
+            )}
+          />
+        </FormSectionsWrapper>
 
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-            </FormWrapper>
+        <FormSectionsWrapper title="Contact info">
+          <FormField
+            control={form.control}
+            name="phoneNumber"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Phone number*</FormLabel>
+                <FormControl>
+                  <PhoneInput
+                    enableSearch={true}
+                    buttonStyle={{ border: "none" }}
+                    containerStyle={{
+                      borderRadius: "7px",
+                      paddingBlock: 3,
+                      width: "100%",
+                      border: "0.4px #ECECEC solid",
+                    }}
+                    inputStyle={{
+                      border: "none",
+                      width: "100%",
+                      backgroundColor: "transparent",
+                    }}
+                    value={form.getValues("phoneNumber")}
+                    onChange={(phone) => form.setValue("phoneNumber", phone)}
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="whatsApp"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>WhatsApp number*</FormLabel>
+                <FormControl>
+                  <PhoneInput
+                    enableSearch={true}
+                    buttonStyle={{ border: "none" }}
+                    containerStyle={{
+                      borderRadius: "7px",
+                      paddingBlock: 3,
+                      width: "100%",
+                      border: "0.4px #ECECEC solid",
+                    }}
+                    inputStyle={{
+                      border: "none",
+                      width: "100%",
+                      backgroundColor: "transparent",
+                    }}
+                    value={form.getValues("whatsApp")}
+                    onChange={(phone) => form.setValue("whatsApp", phone)}
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </FormSectionsWrapper>
+        <FormSectionsWrapper title="Gallery & logo">
+          <FormField
+            control={form.control}
+            name="logo"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Logo*</FormLabel>
+                <div className="flex items-center gap-3 w-full flex-wrap">
+                  <FormControl>
+                    <SingleImageDropzone
+                      width={200}
+                      height={200}
+                      value={file}
+                      onChange={(file) => {
+                        setFile(file);
+                      }}
+                    />
+                  </FormControl>
+                  <Button
+                    disabled={!file || !!form.watch("logo")}
+                    type="button"
+                    onClick={uploadImage}
+                  >
+                    Upload
+                  </Button>
+                </div>
+                <ImagePlaceholder />
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="gallary"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Add images</FormLabel>
+                <div className="flex items-center gap-3 w-full flex-wrap">
+                  <FormControl>
+                    <SingleImageDropzone
+                      width={200}
+                      height={200}
+                      value={imagesFile}
+                      onChange={(imagesFile) => {
+                        setImagesFile(imagesFile);
+                      }}
+                    />
+                  </FormControl>
+                  <Button
+                    disabled={!imagesFile}
+                    type="button"
+                    onClick={uploadImages}
+                  >
+                    Upload
+                  </Button>
+                </div>
+                <ImagesPlaceholder />
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </FormSectionsWrapper>
 
         {logOut && (
           <div className="p-6 font-medium border-yellow-500 bg-yellow-500/20 text-muted-foreground w-full border text-sm">
