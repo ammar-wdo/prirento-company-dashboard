418d14df34a26cb993a382bcd1458ac98e0b39a9
ammar <ammar@wdodigital.com>
2024-03-05 08:33:26 +0400

    seo

---
+++ app/api/(public)/cars/cars-seo/route.ts
@@ -0,0 +1,51 @@
+import { CustomError } from "@/costum-error";
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+
+
+//fetch  all companys for seo
+
+export const GET = async (
+  req: Request,
+
+) => {
+  try {
+    const apiSecret = req.headers.get("api-Secret");
+
+    if (!apiSecret || apiSecret !== process.env.API_SECRET)
+      throw new CustomError("Unauthorized");
+
+
+  
+
+    const cars = await prisma.car.findMany({
+    
+      select:{
+        id:true,
+      slug:true,
+      company:{
+        select:{
+            slug:true
+        }
+      }
+      }
+    
+    });
+
+
+return NextResponse.json({success:true,cars},{status:200})
+
+
+
+
+
+  } catch (error) {
+    let message = "Internal server error";
+    if (error instanceof CustomError) message = error.message;
+    return NextResponse.json(
+      { success: false, error: message, company: null },
+      { status: 200 }
+    );
+  }
+};
---
+++ app/api/(public)/company/route.ts
@@ -0,0 +1,46 @@
+import { CustomError } from "@/costum-error";
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+
+
+//fetch  all companys for seo
+
+export const GET = async (
+  req: Request,
+
+) => {
+  try {
+    const apiSecret = req.headers.get("api-Secret");
+
+    if (!apiSecret || apiSecret !== process.env.API_SECRET)
+      throw new CustomError("Unauthorized");
+
+
+  
+
+    const companies = await prisma.company.findMany({
+    
+      select:{
+        id:true,
+      slug:true
+      }
+    
+    });
+
+
+return NextResponse.json({success:true,companies},{status:200})
+
+
+
+
+
+  } catch (error) {
+    let message = "Internal server error";
+    if (error instanceof CustomError) message = error.message;
+    return NextResponse.json(
+      { success: false, error: message, company: null },
+      { status: 200 }
+    );
+  }
+};
