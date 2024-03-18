4e690791b34dcb3ed5d256a8eadbe0bb2bfa594d
ammar <ammar@wdodigital.com>
2024-03-18 09:43:33 +0400

    refine

--- app/api/(native)/native/bookings/[bookingId]/route.tsx
+++ app/api/(native)/native/bookings/[bookingId]/route.tsx
@@ -43,7 +43,8 @@ const booking = await prisma.booking.findUnique({
         car:{
             company:{
                 email:decoded.email
-            }
+            },
+            
         }
     }
     ,
---
+++ app/api/(native)/native/car/[carId]/extraOptions/route.tsx
@@ -0,0 +1,72 @@
+import { CustomError } from "@/costum-error";
+import prisma from "@/lib/prisma";
+import { areIdsValid, verifyToken } from "@/lib/utils";
+import { carSchema } from "@/schemas";
+import { NextResponse } from "next/server";
+
+export const revalidate = 0;
+
+
+
+
+export const GET = async (
+  req: Request,
+  { params }: { params: { carId: string } }
+) => {
+
+  try {
+    const apiSecret = req.headers.get("api-Secret"); //API secret key to prevent 3rd party requests
+
+    if (!apiSecret || apiSecret !== process.env.API_SECRET) {
+      throw new CustomError("Unauthorized request");
+    }
+
+    if (!params.carId) throw new CustomError("Car Id is required");
+
+    const authHeader = req.headers.get("Authorization");
+
+    if (!authHeader || !authHeader.startsWith("Bearer "))
+      throw new CustomError("Not Authorized");
+
+    const token = authHeader.split(" ")[1];
+
+    const decoded = verifyToken(token);
+
+    if (!decoded) throw new CustomError("Not Authorized");
+
+
+
+
+
+
+    const extraOptions = await prisma.carExtraOption.findMany({
+      where:{
+      carId:params.carId,
+      car:{
+        company:{
+            email:decoded.email
+        }
+      }
+       
+      }
+    })
+
+    
+
+  
+
+
+
+
+    return  NextResponse.json({success:true,extraOptions},{status:201});
+  } catch (error) {
+    let message = "Something went wrong";
+    if (error instanceof CustomError) {
+      message = error.message;
+    }
+    console.log(error);
+    return NextResponse.json({success:false,message});
+  }
+
+
+}
