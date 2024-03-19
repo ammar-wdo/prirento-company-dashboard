14ccab945932abe63e2d4696a12404206de9c4d8
ammar <ammar@wdodigital.com>
2024-03-19 08:25:07 +0400

    notifications

--- app/api/(public)/webhook/route.ts
+++ app/api/(public)/webhook/route.ts
@@ -5,20 +5,13 @@ import { NextResponse } from "next/server";
 import { stripe } from "@/lib/stripe";
 import prisma from "@/lib/prisma";
 
-
 export async function POST(req: Request) {
-
-  console.log('webhook')
+  console.log("webhook");
   const body = await req.text();
-  
 
   const signature = headers().get("Stripe-Signature") as string;
 
-
   let event: Stripe.Event;
-
-
-
 
   try {
     event = stripe.webhooks.constructEvent(
@@ -26,65 +19,115 @@ export async function POST(req: Request) {
       signature,
       process.env.STRIPE_WEBHOOK_SECRET!
     );
-
-
   } catch (error: any) {
-    console.log(error)
+    console.log(error);
     return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 });
-  
   }
 
   const session = event.data.object as Stripe.Checkout.Session;
 
   switch (event.type) {
-
     //checkout completed
     case "checkout.session.completed": {
-        try {
-          if (session.payment_status === "paid") {
-            const order = await prisma.booking.update({
-              where: {
-                id: session?.metadata?.id,
-              },
-              data: {
-                paymentStatus: "SUCCEEDED",
-              },
-            
-            });
-      
-
-          }
-        } catch (error) {
-          console.log(error);
-        }
-  
-
-      break;
-    }
-//checkout expired
-    case "checkout.session.expired": {
- 
-      
-        try {
+      try {
+        if (session.payment_status === "paid") {
           const order = await prisma.booking.update({
             where: {
               id: session?.metadata?.id,
             },
             data: {
-              paymentStatus: "EXPIRED",
-              bookingStatus: "CANCELED",
+              paymentStatus: "SUCCEEDED",
             },
-        
+            include: {
+              car: {
+                select: {
+                  carModel: {
+                    select: {
+                      name: true,
+                      carBrand: {
+                        select: {
+                          brand: true,
+                        },
+                      },
+                    },
+                  },
+                  company: {
+                    select: {
+                      id: true,
+                    },
+                  },
+                },
+              },
+            },
           });
 
-       
-        } catch (error) {
-          console.log(error);
+          
+   //trigger notification
+          await prisma.notification.create({
+            data: {
+              bookingId: order.id,
+              companyId: order.car.company.id,
+              message: `Someone booked ${order.car.carModel.carBrand.brand} ${order.car.carModel.name}`,
+              type: "BOOKING",
+            },
+          });
         }
-   
+      } catch (error) {
+        console.log(error);
+      }
+
       break;
     }
-//checkout refunded
+    //checkout expired
+    case "checkout.session.expired": {
+      try {
+        const order = await prisma.booking.update({
+          where: {
+            id: session?.metadata?.id,
+          },
+          data: {
+            paymentStatus: "EXPIRED",
+            bookingStatus: "CANCELED",
+          },
+          include: {
+            car: {
+              select: {
+                carModel: {
+                  select: {
+                    name: true,
+                    carBrand: {
+                      select: {
+                        brand: true,
+                      },
+                    },
+                  },
+                },
+                company: {
+                  select: {
+                    id: true,
+                  },
+                },
+              },
+            },
+          },
+        });
+
+        //trigger notification
+        await prisma.notification.create({
+          data: {
+            bookingId: order.id,
+            companyId: order.car.company.id,
+            message: `Someone booked ${order.car.carModel.carBrand.brand} ${order.car.carModel.name}`,
+            type: "BOOKING",
+          },
+        });
+      } catch (error) {
+        console.log(error);
+      }
+
+      break;
+    }
+    //checkout refunded
     case "charge.refunded": {
       try {
         const order = await prisma.booking.update({
@@ -95,7 +138,6 @@ export async function POST(req: Request) {
             paymentStatus: "CANCELED",
             bookingStatus: "REFUNDED",
           },
-        
         });
       } catch (error) {
         console.log(error);
