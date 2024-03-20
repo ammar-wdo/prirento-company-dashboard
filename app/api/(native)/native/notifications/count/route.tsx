0532ee155457d78ed65736679264bbfdfd62ee50
ammar <ammar@wdodigital.com>
2024-03-20 10:18:39 +0400

    refine

---
+++ app/api/(native)/native/notifications/count/route.tsx
@@ -0,0 +1,59 @@
+import { CustomError } from "@/costum-error";
+import prisma from "@/lib/prisma";
+import { verifyToken } from "@/lib/utils";
+import { NextResponse } from "next/server";
+
+export const GET = async (
+    req: Request,
+    { params }: { params: { carId: string ,optionId:string} }
+  ) => {
+  
+    try {
+      const apiSecret = req.headers.get("api-Secret"); //API secret key to prevent 3rd party requests
+  
+      if (!apiSecret || apiSecret !== process.env.API_SECRET) {
+        throw new CustomError("Unauthorized request");
+      }
+  
+      if (!params.carId) throw new CustomError("Car Id is required");
+    
+  
+      const authHeader = req.headers.get("Authorization");
+  
+      if (!authHeader || !authHeader.startsWith("Bearer "))
+        throw new CustomError("Not Authorized");
+  
+      const token = authHeader.split(" ")[1];
+  
+      const decoded = verifyToken(token);
+  
+      if (!decoded) throw new CustomError("Not Authorized");
+  
+  
+  
+  
+  
+  
+      const count = await prisma.notification.count({
+        where:{
+  
+        company:{
+          email:decoded.email
+        },isRead:false
+        
+         
+        }
+      })
+  
+      return  NextResponse.json({success:true,count},{status:201});
+    } catch (error) {
+      let message = "Something went wrong";
+      if (error instanceof CustomError) {
+        message = error.message;
+      }
+      console.log(error);
+      return NextResponse.json({success:false,error:message});
+    }
+  
+  
+  }
