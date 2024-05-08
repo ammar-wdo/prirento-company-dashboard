4508308ae8cfab748156abf298f44e0235ac1992
ammar <ammar@wdodigital.com>
2024-05-08 09:43:12 +0400

    logout

---
+++ app/api/(native)/native/check/route.ts
@@ -0,0 +1,51 @@
+import { CustomError } from "@/costum-error";
+import prisma from "@/lib/prisma";
+import { dubaiTimeZone, verifyToken } from "@/lib/utils";
+import { NextResponse } from "next/server";
+
+
+export const revalidate = 0
+export const GET = async (req:Request)=>{
+
+
+    try {
+
+        const apiSecret = req.headers.get("api-Secret"); //API secret key to prevent 3rd party requests
+
+        if (!apiSecret || apiSecret !== process.env.API_SECRET) {
+          throw new CustomError("Unauthorized request");
+        }
+
+        const authHeader = req.headers.get('Authorization')
+        console.log('header',authHeader)
+        if (!authHeader || !authHeader.startsWith('Bearer ')) throw new CustomError('Not Authorized')
+
+        const token = authHeader.split(' ')[1];
+        
+        const decoded = verifyToken(token)
+
+        if(!decoded) throw new CustomError('Not Authorized')
+        console.log('email',decoded.email)
+
+const company = await prisma.company.findUnique({
+    where:{
+        email:decoded.email
+    }
+})
+
+if(!company) return NextResponse.json({success:false,logout:true},{status:200})
+
+return NextResponse.json({success:true,logout:false},{status:200})
+
+
+
+
+        
+    } catch (error) {
+        let message = 'Internal server error'
+
+        if(error instanceof CustomError)message = error.message
+
+        return NextResponse.json({success:false,error:message},{status:200})
+    }
+}
