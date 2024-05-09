d315133b3efd46f3aea5299d33f906822855ae55
ammar <ammar@wdodigital.com>
2024-05-09 10:29:35 +0400

    refine

--- app/api/(public)/faq/route.ts
+++ app/api/(public)/faq/route.ts
@@ -1,20 +1,24 @@
 import { CustomError } from "@/costum-error";
 import prisma from "@/lib/prisma";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export const revalidate = 0
 
-export const GET = async(req:Request)=>{
+export const GET = async(req:NextRequest)=>{
 
     try {
         const apiSecret = req.headers.get('api-Secret')
         
         if(!apiSecret || apiSecret !== process.env.API_SECRET) throw new CustomError("Unauthorized")
 
+        const searchParams = req.nextUrl.searchParams
+        const main = searchParams.get('main')
+
         const faqs = await prisma.faq.findMany({
            orderBy:{
             createdAt:'desc'
-           }
+           },
+           ...(!!(main==='true') && {take:5})
         })
 
 
